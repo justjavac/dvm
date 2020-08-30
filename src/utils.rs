d7921a5f3eb743f119f7bc96b57b7fedf7957413
justjavac <justjavac@gmail.com>
2020-08-30 22:22:01 +0800

    2

--- src/utils.rs
+++ src/utils.rs
@@ -6,18 +6,17 @@ use std::env;
 use std::path::PathBuf;
 
 pub fn get_dvm_root() -> Result<PathBuf> {
-    // Note: on Windows, the $HOME environment variable may be set by users or by
-    // third party software, but it is non-standard and should not be relied upon.
-    let home_env_var = if cfg!(windows) { "USERPROFILE" } else { "HOME" };
-    let mut home_path = match env::var_os(home_env_var).map(PathBuf::from) {
-      Some(home_path) => home_path,
-      None => {
-        // Use temp dir
-        TempDir::new()?.into_path()
-      }
-    };
-  
-    home_path.push(".dvm");
-    Ok(home_path)
-  }
-  
+  // Note: on Windows, the $HOME environment variable may be set by users or by
+  // third party software, but it is non-standard and should not be relied upon.
+  let home_env_var = if cfg!(windows) { "USERPROFILE" } else { "HOME" };
+  let mut home_path = match env::var_os(home_env_var).map(PathBuf::from) {
+    Some(home_path) => home_path,
+    None => {
+      // Use temp dir
+      TempDir::new()?.into_path()
+    }
+  };
+
+  home_path.push(".dvm");
+  Ok(home_path)
+}
