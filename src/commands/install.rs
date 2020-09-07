20e1f30586bb778ad2b0631890d663cb309ba3d1
迷渡 <justjavac@gmail.com>
2020-09-07 09:41:43 +0800

    fix test

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -194,12 +194,12 @@ fn test_compose_url_to_exec() {
   let v = semver_parse("0.0.1").unwrap();
   let url = compose_url_to_exec(&v).unwrap();
   #[cfg(windows)]
-  assert_eq!(url.as_str(), "https://github.com/denoland/deno/releases/download/v0.0.1/deno-x86_64-pc-windows-msvc.zip");
+  assert_eq!(url.as_str(), "https://cdn.jsdelivr.net/gh/justjavac/deno_releases/0.0.1/deno-x86_64-pc-windows-msvc.zip");
   #[cfg(target_os = "macos")]
   assert_eq!(
     url.as_str(),
-    "https://github.com/denoland/deno/releases/download/v0.0.1/deno-x86_64-apple-darwin.zip"
+    "https://cdn.jsdelivr.net/gh/justjavac/deno_releases/0.0.1/deno-x86_64-apple-darwin.zip"
   );
   #[cfg(target_os = "linux")]
-  assert_eq!(url.as_str(), "https://github.com/denoland/deno/releases/download/v0.0.1/deno-x86_64-unknown-linux-gnu.zip");
+  assert_eq!(url.as_str(), "https://cdn.jsdelivr.net/gh/justjavac/deno_releases/0.0.1/deno-x86_64-unknown-linux-gnu.zip");
 }
