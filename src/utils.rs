e1e53a9630993ecca22298cad33ac3b620685add
Chawye Hsu <chawyehsu@hotmail.com>
2020-12-22 15:19:40 +0800

    feat: Support changing dvm root by DVM_DIR env
    Signed-off-by: Chawye Hsu <chawyehsu@hotmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -7,19 +7,23 @@ use std::fs;
 use std::path::PathBuf;
 
 pub fn get_dvm_root() -> PathBuf {
-  // Note: on Windows, the $HOME environment variable may be set by users or by
-  // third party software, but it is non-standard and should not be relied upon.
-  let home_env_var = if cfg!(windows) { "USERPROFILE" } else { "HOME" };
-  let mut home_path = match env::var_os(home_env_var).map(PathBuf::from) {
-    Some(home_path) => home_path,
+  match env::var_os("DVM_DIR").map(PathBuf::from) {
+    Some(dvm_dir) => dvm_dir,
     None => {
-      // Use temp dir
-      TempDir::new().unwrap().into_path()
+      // Note: on Windows, the $HOME environment variable may be set by users or by
+      // third party software, but it is non-standard and should not be relied upon.
+      let home_env_var = if cfg!(windows) { "USERPROFILE" } else { "HOME" };
+      let mut home_path = match env::var_os(home_env_var).map(PathBuf::from) {
+        Some(home_path) => home_path,
+        None => {
+          // Use temp dir
+          TempDir::new().unwrap().into_path()
+        }
+      };
+      home_path.push(".dvm");
+      home_path
     }
-  };
-
-  home_path.push(".dvm");
-  home_path
+  }
 }
 
 pub fn get_exe_path(version: &Version) -> PathBuf {
