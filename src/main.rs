87d0abc5840df0e4355704faf06a0f1e49286efb
迷渡 <justjavac@gmail.com>
2021-01-29 12:39:49 +0800

    use tinyget instead of reqwest ureq
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate anyhow;
+extern crate clap;
+// extern crate getopts;
+extern crate tinyget;
+extern crate semver_parser;
+extern crate tempfile;
+extern crate which;
+
 mod commands;
 mod flags;
 mod utils;
