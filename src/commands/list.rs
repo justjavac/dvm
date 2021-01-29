0e3a14a51f77f27329aa2667c08974b0ee15fb71
yelqgd <yelqgd@gmail.com>
2021-01-30 00:54:33 +0800

    feat: Highlight the current used version in versions list

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 
-use crate::version::{get_local_versions, get_remote_versions};
+use crate::version::{
+  get_current_version, get_local_versions, get_remote_versions,
+};
 
 pub fn exec() -> Result<()> {
   let versions = get_local_versions();
@@ -17,7 +19,19 @@ pub fn exec_remote() -> Result<()> {
 }
 
 fn print_versions(mut versions: Vec<String>) {
+  let current_version = match get_current_version() {
+    Some(v) => v,
+    _ => String::from(""),
+  };
   versions.sort();
   versions.reverse();
-  println!("{}", versions.join("\n"));
+
+  for v in &versions {
+    if v.to_string() == current_version {
+      // display current used version with bright green
+      println!("\x1b[0;92m*{}\x1b[0m", v);
+    } else {
+      println!(" {}", v)
+    }
+  }
 }
