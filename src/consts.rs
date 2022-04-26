928c96dbbb985cf7aa6978e4241a644ede559a3d
CGQAQ <m.jason.liu@outlook.com>
2022-04-26 15:09:26 +0800

    make clippy happy

--- src/consts.rs
+++ src/consts.rs
@@ -1,6 +1,6 @@
-pub const REGISTRY_OFFICIAL: &'static str = "https://dl.deno.land/";
+pub const REGISTRY_OFFICIAL: &str = "https://dl.deno.land/";
 #[allow(unused)]
-pub const REGISTRY_CN: &'static str = "https://dl.deno.js.cn/";
-pub const REGISTRY_LATEST_RELEASE_PATH: &'static str = "release-latest.txt";
+pub const REGISTRY_CN: &str = "https://dl.deno.js.cn/";
+pub const REGISTRY_LATEST_RELEASE_PATH: &str = "release-latest.txt";
 
-pub const DVM_BIN_PATH_PREFIX: &'static str = "versions";
+pub const DVM_BIN_PATH_PREFIX: &str = "versions";
