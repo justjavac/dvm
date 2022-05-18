3cd2aa1a740af2b439250019118c467b1d31ad73
CGQAQ <m.jason.liu@outlook.com>
2022-05-18 17:04:55 +0800

    migrates from old cache

--- src/commands/doctor.rs
+++ src/commands/doctor.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use colored::Colorize;
+use std::fs;
 
 use crate::{
   meta::DvmMeta,
-  utils::{deno_bin_path, dvm_root},
+  utils::{deno_bin_path, dvm_root, is_exact_version},
 };
 
 pub fn exec(meta: &mut DvmMeta) -> Result<()> {
@@ -18,9 +19,25 @@ pub fn exec(meta: &mut DvmMeta) -> Result<()> {
     println!("{}", "Please restart your shell of choice to take effects.".red());
   }
 
+  // migrating from old dvm cache.
+  let list = fs::read_dir(home_path).unwrap();
+  for entry in list {
+    let entry = entry.unwrap();
+    let path = entry.path();
+    if path.is_dir() {
+      let name = path.file_name().unwrap().to_str().unwrap();
+      if is_exact_version(name) {
+        // move to `versions` subdir
+        println!("Found old dvm cache of version `{}`, migrating to new dvm cache location...", name);
+        fs::rename(path.clone(), path.parent().unwrap().join("versions").join(name)).unwrap();
+      }
+    }
+  }
+
   if !dirs::home_dir().unwrap().join(".dvmrc").exists() {
     super::use_version::exec(meta, None).unwrap();
   }
+
 
   println!("{}", "All fixes applied, DVM is ready to use.".green());
   Ok(())
