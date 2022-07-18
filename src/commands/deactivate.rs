79b9db5beade8b9877e69030cad9fe1f5fc6720b
Jason <m.jason.liu@outlook.com>
2022-07-18 11:54:00 +0800

    feat(use): system alias (#128)

--- src/commands/deactivate.rs
+++ src/commands/deactivate.rs
@@ -1,14 +1,18 @@
 use crate::utils::check_is_deactivated;
 use crate::{deno_bin_path, dvm_root};
-use anyhow::{anyhow, Result};
+use anyhow::{Ok, Result};
 
 pub fn exec() -> Result<()> {
   let home = dvm_root();
-  if !check_is_deactivated() {
-    std::fs::write(home.join(".deactivated"), "").unwrap();
+  if check_is_deactivated() {
+    println!("Dvm has already been deactivated, exiting.");
+    return Ok(());
   }
 
-  std::fs::remove_file(deno_bin_path())
-    .map(|_| ())
-    .map_err(|e| anyhow!(e))
+  std::fs::write(home.join(".deactivated"), "").unwrap();
+  std::fs::remove_file(deno_bin_path()).unwrap();
+
+  println!("Dvm is now deacvated.");
+  println!("Deno that was previously installed on your system will be activated now.");
+  Ok(())
 }
