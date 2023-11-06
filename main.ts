464e326f3d72a88bdbbc564a4638c5cd6f55ab93
迷渡 <justjavac@gmail.com>
2023-11-06 14:55:10 +0800

    upgrade deno std@0.152.0

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.205.0/http/server.ts";
 
 serve(async (req: Request) => {
   const userAgent = req.headers.get("User-Agent") || "";
